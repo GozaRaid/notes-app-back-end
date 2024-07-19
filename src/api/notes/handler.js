e52a4fc194ca2b981876f21122461eeb4b88991d
GozaRaid <hafidzhitobaihaqi@gmail.com>
2024-07-19 21:10:32 +0700

    user registration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -71,7 +71,7 @@ class NotesHandler {
 
     const response = h.response({
       status: 'success',
-      message: 'Catatan berhasil diperbarui',
+      message: 'Catatan berhasil dihapus',
     });
     response.code(200);
     return response;
