0159bb8ece80304a952f3fcb4810b217d5cd4a51
GozaRaid <hafidzhitobaihaqi@gmail.com>
2024-07-21 17:08:32 +0700

    authentication implemented

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -10,7 +10,8 @@ class UsersHandler {
 
   async postUserHandler(request, h) {
     this._validator.validateUserPayload(request.payload);
-    const userId = await this._service.addUser(request.payload);
+    const { username, password, fullname } = request.payload;
+    const userId = await this._service.addUser({ username, password, fullname });
     const response = h.response({
       status: 'success',
       message: 'User berhasil ditambahkan',
