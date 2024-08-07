989d70a1fd85822d64a4e721679f2858815fcf76
GozaRaid <hafidzhitobaihaqi@gmail.com>
2024-08-07 23:22:53 +0700

    caching server-side implemented

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -3,8 +3,9 @@ const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
 
 class CollaborationsService {
-  constructor() {
+  constructor(cacheService) {
     this._pool = new Pool();
+    this._chaceService = cacheService;
   }
 
   async addCollaboration(noteId, userId) {
@@ -20,6 +21,8 @@ class CollaborationsService {
     if (!result.rows.length) {
       throw new InvariantError('Kolaborasi gagal ditambahkan');
     }
+
+    await this._chaceService.delete(`notes:${userId}`);
     return result.rows[0].id;
   }
 
@@ -34,6 +37,8 @@ class CollaborationsService {
     if (!result.rows.length) {
       throw new InvariantError('Kolaborasi gagal dihapus');
     }
+
+    await this._chaceService.delete(`notes:${userId}`);
   }
 
   async verifyCollaborator(noteId, userId) {
